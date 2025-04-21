06fc48d709e2f22083b770e28bde81f7a2c350bf
David Schor <18281740+david-schor@users.noreply.github.com>
2025-04-21 21:55:43 +0200

    add jetbrains-mono font

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -4,7 +4,8 @@
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
-    (nerdfonts.override { fonts = [  "FiraCode" ]; })
+    (nerdfonts.override { fonts = [  "FiraCode" "JetBrainsMono" ]; })
     fira-code
+    jetbrains-mono
   ];
 }
