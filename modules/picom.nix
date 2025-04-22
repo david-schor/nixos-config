ef3f8971baf37fa94dc27dcac73b8846d473a0bb
David Schor <18281740+david-schor@users.noreply.github.com>
2025-04-22 19:30:50 +0200

    remove transparency & blur

--- modules/picom.nix
+++ modules/picom.nix
@@ -3,8 +3,8 @@
 {
   services.picom = {
     enable = true;
-    activeOpacity = 0.8;
-    inactiveOpacity = 0.6;
+#    activeOpacity = 0.8;
+#    inactiveOpacity = 0.6;
     backend = "glx";
     settings = {
       corner-radius = 8;
@@ -12,10 +12,10 @@
       vsync = true;
       refresh-rate = 180;
       xrender-sync-fence = false;
-      blur = {
-        method = "dual_kawase";
-        size = 10;
-      };
+#      blur = {
+#        method = "dual_kawase";
+#        size = 10;
+#      };
       fade = true;
       rounded-corners-exclude = [
         "class_i = 'polybar'"
