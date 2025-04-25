ebb7424b8a743781f718c32d829b4fab2b2b6eeb
David Schor <18281740+david-schor@users.noreply.github.com>
2025-04-25 16:19:19 +0200

    refactor vars

---
+++ modules/config/colors.nix
@@ -0,0 +1,25 @@
+{
+  background = "#25262A";
+  foreground = "#E2E2E2";
+  cursor = "#E2E2E2";
+  select-back = "#3E3F44";
+  select-fore = "#ffffff";
+ 
+  black = "#2E2F33";
+  red = "#D88C8C";
+  green = "#A7D9AF";
+  yellow = "#E9D8AD";
+  blue = "#AFC4EA";
+  magenta = "#D5B5F2";
+  cyan = "#A4DAE0";
+  gray = "#E2E2E2";
+
+  gray-alt = "#3D3E42";
+  red-alt = "#EDA3A3";
+  green-alt = "#BAE6C0";
+  yellow-alt = "#F6E8B8";
+  blue-alt = "#C1D1F3";
+  magenta-alt = "#E0C3F7";
+  cyan-alt = "#B8E4EA";
+  white = "#ffffff";
+}
--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -1,37 +1,44 @@
 { pkgs, ... }:
 
+let
+  colors = import ./config/colors.nix;
+  config = import ./config/config.nix;
+in
 {
   programs.ghostty = {
     enable = true;
     settings = {
-      theme = "twilight-rain";
+      cursor-color = colors.cursor;
+      shell-integration = config.shell;
+      font-size = 12;
+      theme = "mountain-dawn";
       window-decoration = false;
     };
     themes = {
-      twilight-rain = {
+      mountain-dawn = {
+        background = colors.background;
+        foreground = colors.foreground;
+        cursor-color = colors.cursor;
+        selection-background = colors.select-back;
+        selection-foreground = colors.select-fore;
         palette = [
-          "0=#0d1b2a"
-          "1=#e26a6a"
-          "2=#94d2bd"
-          "3=#ffc49b"
-          "4=#7d91b7"
-          "5=#ff8e72"
-          "6=#a2bfe5"
-          "7=#e0e1dd"
-          "8=#1b263b"
-          "9=#f07178"
-          "10=#b5ead7"
-          "11=#ffe0b2"
-          "12=#a2bfe5"
-          "13=#ffb4a2"
-          "14=#94e2d5"
-          "15=#ffffff"
+          "0=${colors.black}"
+          "1=${colors.red}"
+          "2=${colors.green}"
+          "3=${colors.yellow}"
+          "4=${colors.blue}"
+          "5=${colors.magenta}"
+          "6=${colors.cyan}"
+          "7=${colors.gray}"
+          "8=${colors.gray-alt}"
+          "9=${colors.red-alt}"
+          "10=${colors.green-alt}"
+          "11=${colors.yellow-alt}"
+          "12=${colors.blue-alt}"
+          "13=${colors.magenta-alt}"
+          "14=${colors.cyan-alt}"
+          "15=${colors.white}"
         ];
-        background = "0d1b2a";
-        foreground = "e0e1dd";
-        cursor-color = "ff8e72";
-        selection-background = "1b263b";
-        selection-foreground = "ffffff";
       };
     };
   };
