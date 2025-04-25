f5737a36997aa3de6c00f1813527a2d14d015f8a
David Schor <18281740+david-schor@users.noreply.github.com>
2025-04-25 22:49:21 +0200

    tabs

--- modules/neovim/plugins/lsp.lua
+++ modules/neovim/plugins/lsp.lua
@@ -2,22 +2,22 @@ local lspconfig = require("lspconfig")
 local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 lspconfig.lua_ls.setup({
-  capabilities = lsp_capabilities,
+	capabilities = lsp_capabilities,
 })
 lspconfig.gopls.setup({
-  capabilities = lsp_capabilities,
+	capabilities = lsp_capabilities,
 })
 lspconfig.pyright.setup({
-  capabilities = lsp_capabilities,
+	capabilities = lsp_capabilities,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  desc = "LSP actions",
-  callback = function()
-    local bufmap = function(mode, lhs, rhs)
-      local opts = { buffer = true }
-      vim.keymap.set(mode, lhs, rhs, opts)
-    end
+	desc = "LSP actions",
+	callback = function()
+		local bufmap = function(mode, lhs, rhs)
+		local opts = { buffer = true }
+		vim.keymap.set(mode, lhs, rhs, opts)
+	end
 
     bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
     bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
@@ -31,15 +31,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
     bufmap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
     bufmap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
     bufmap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
-  end,
+	end,
 })
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-  vim.lsp.handlers.hover,
-  { border = "rounded" }
+	vim.lsp.handlers.hover,
+	{ border = "rounded" }
 )
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-  vim.lsp.handlers.signature_help,
-  { border = "rounded" }
+	vim.lsp.handlers.signature_help,
+	{ border = "rounded" }
 )
